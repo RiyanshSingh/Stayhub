44821fcb78210adf8b2c999209c581b9907d8d8e
Riyansh Singh <itsyourriyansh@gmail.com>
2025-12-16 12:21:43 +0530

    Updated

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 import app from '../server/index.js';
 
 export default function handler(req, res) {
-    return app(req, res);
+    // Vercel/Webpack sometimes wraps the default export in another default property
+    const expressApp = app.default || app;
+
+    if (typeof expressApp === 'function') {
+        return expressApp(req, res);
+    }
+
+    console.error("Express App is not a function:", typeof expressApp, expressApp);
+    res.status(500).json({ error: "Server Initialization Failed" });
 }
